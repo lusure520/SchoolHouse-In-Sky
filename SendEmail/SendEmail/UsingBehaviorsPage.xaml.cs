bd2f8df5753361a855cb7d218f97e8ecc6a2ec0d
Yuanlu Deng <dengyuanlu@gmail.com>
2018-09-29 00:04:35 +1200

    initial master
    User Story1

---
+++ SendEmail/SendEmail/UsingBehaviorsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SendEmail
+{
+    public partial class HelloXamlPage : ContentPage
+    {
+        public HelloXamlPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SendEmail/SendEmail/Views/SignUp.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SendEmail.Views
+{
+    public partial class SignUp : ContentPage
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
