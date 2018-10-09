3369bfc484c9305b0f3949a9ffc8a9e96e5caf48
MBigeni <37860866+MBigeni@users.noreply.github.com>
2018-10-09 21:19:18 +1300

    Home page and editing page
    Home page displays content and editing page to allow content to be edited.

--- SendEmail/SendEmail/App.xaml.cs
+++ SendEmail/SendEmail/App.xaml.cs
@@ -11,7 +11,7 @@ namespace SendEmail
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Home());
         }
 
         protected override void OnStart()
