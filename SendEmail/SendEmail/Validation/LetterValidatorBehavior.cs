3076797f2bbf3b1e6bb88b6af2fc0f44364f0ef0
Yuanlu Deng <dengyuanlu@gmail.com>
2018-10-18 00:33:55 +1300

    validation for input
    validation

---
+++ SendEmail/SendEmail/Validation/LetterValidatorBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace SendEmail.Validation
+{
+    public class LetterValidatorBehavior : Behavior<Entry>
+    {
+        const string numberRegex = @"^[a-zA-Z]+$";
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (!string.IsNullOrWhiteSpace(args.NewTextValue))
+            {
+                bool IsValid = (Regex.IsMatch(args.NewTextValue, numberRegex, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250)));
+                ((Entry)sender).Text = IsValid ? args.NewTextValue : args.NewTextValue.Remove(args.NewTextValue.Length - 1);
+            }
+        }
+    }
+}
+
